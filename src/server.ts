d80840b74fc4dd75099dd94e4b47520fef97683f
Oscar Hernandez <os.hf024@gmail.com>
2021-09-06 17:43:43 -0500

    build: Add handlebars

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
+import exphbs from 'express-handlebars';
 
 // Initializations
 const app: Application = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '../src/views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 app.all('/favicon.ico', (req, res) => res.status(204).end());
 
@@ -18,7 +28,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 export default app;
